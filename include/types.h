8bc663eaab69e47dcfa03d34af984da3f79e7981
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-11-26 16:41:32 +0100

    Patched the types, so they now contain the expressions and operators are correctly parsed

--- include/types.h
+++ include/types.h
@@ -3,96 +3,98 @@
 #include <string>
 #include <utility>
 
-/* Used for debugging */ 
-template<typename T>
-struct TypeStringable {
-	virtual std::string GetTypeString() { return typeid(T).name(); }
-};
-
-struct Expression : TypeStringable<Expression> {
+struct Expression {
 	std::string expression_string;
 	explicit Expression(std::string  str) : expression_string{std::move(str)} {}
 };
 
-struct Literal : Expression, TypeStringable<Literal> { 
+struct Literal : Expression {
 	explicit Literal(const std::string& str) : Expression{str} {}
 };
 
-struct VariableIdentifier : Expression, TypeStringable<VariableIdentifier> { 
-	explicit VariableIdentifier(const std::string& str) : Expression{str} {}
+struct VariableIdentifier : Expression {
+    explicit VariableIdentifier(const std::string& str) : Expression{str} {}
 };
 
-struct LocationIdentifier : Expression, TypeStringable<LocationIdentifier> {
+struct LocationIdentifier : Expression {
 	explicit LocationIdentifier(const std::string& str) : Expression{str} {}
 };
 
-struct Deadlock : Expression, TypeStringable<Deadlock> { 
+struct Deadlock : Expression {
 	explicit Deadlock(const std::string& str) : Expression{str} {}
 };
 
-struct LogicOperator : Expression, TypeStringable<LogicOperator> {
+struct LogicOperator : Expression {
 	enum struct OperatorType {
-		AND, OR
+		AND, OR, UNKNOWN
 	};
 	OperatorType type;
 	Expression left, right;
 	static OperatorType FromString(const std::string& string) {
-		printf("Implement me, please %s\n", string.c_str());
-		return OperatorType::AND;
+        if(string == "&&") return OperatorType::AND;
+		if(string == "and")return OperatorType::AND;
+		if(string == "||") return OperatorType::OR;
+        if(string == "or") return OperatorType::OR;
+		return OperatorType::UNKNOWN; // Something is very wrong, my dude
 	}
 	LogicOperator(const std::string& str, Expression l, Expression r) : Expression{str}, type{FromString(str)}, left{std::move(l)}, right{std::move(r)} {}
 };
 
-struct NegationOperator : Expression, TypeStringable<NegationOperator> {
+struct NegationOperator : Expression {
 	Expression subExpression;
 	explicit NegationOperator(Expression sub) : Expression{"!"}, subExpression{std::move(sub)} {}
 };
 
-struct Comparator : Expression, TypeStringable<Comparator> {
+struct Comparator : Expression {
 	enum struct CompareType {
-		LESSTHAN, LESSTHANEQUAL, NOTEQUAL, EQUAL, GREATERTHAN, GREATERTHANEQUAL
+		LESSTHAN, LESSTHANEQUAL, NOTEQUAL, EQUAL, GREATERTHAN, GREATERTHANEQUAL, UNKNOWN
 	};
 	CompareType type;
 	Expression left, right;
 	static CompareType FromString(const std::string& string) {
-		printf("Implement me, please\n");
-		return CompareType::LESSTHAN;
+        if(string == "<") return CompareType::LESSTHAN;
+        if(string == "<=") return CompareType::LESSTHANEQUAL;
+        if(string == "!=") return CompareType::NOTEQUAL;
+        if(string == "==") return CompareType::EQUAL;
+        if(string == ">=") return CompareType::GREATERTHAN;
+        if(string == ">") return CompareType::GREATERTHANEQUAL;
+		return CompareType::UNKNOWN; // There is something very wrong, my dude
 	}
 	Comparator(const std::string& str, Expression l, Expression r) : Expression{str}, type{FromString(str)}, left{std::move(l)}, right{std::move(r)} {}
 };
 
 /* LPAREN subexpr RPAREN */
-struct SubExpression : Expression, TypeStringable<SubExpression> {
+struct SubExpression : Expression {
 	Expression expr;
 	SubExpression(const std::string& str, Expression e) : Expression{str}, expr{std::move(e)} {}
 };
 
-struct Quantifier : TypeStringable<Quantifier> { 
+struct Quantifier  {
 	Expression expression;
 	explicit Quantifier(Expression e) : expression{std::move(e)} {}
 };
 
-struct Finally : Quantifier, TypeStringable<Finally> {
+struct Finally : Quantifier {
 	explicit Finally(const Expression& e) : Quantifier{e} {}
 };
-struct Globally : Quantifier, TypeStringable<Globally> { 
+struct Globally : Quantifier {
 	explicit Globally(const Expression& e) : Quantifier{e} {}
 };
-struct Next : Quantifier, TypeStringable<Next> { 
+struct Next : Quantifier {
 	explicit Next(const Expression& e) : Quantifier{e} {}
 };
-struct Until : Quantifier, TypeStringable<Until> { 
+struct Until : Quantifier {
 	explicit Until(const Expression& e) : Quantifier{e} {}
 };
 
-struct Query : TypeStringable<Query> { 
+struct Query  {
 	Quantifier quantifier;
 	explicit Query(Quantifier q) : quantifier{std::move(q)} {}
 };
-struct Exists : Query, TypeStringable<Exists> { 
+struct Exists : Query {
 	explicit Exists(const Quantifier& q) : Query{q} {}
 };
-struct Forall : Query, TypeStringable<Forall> { 
+struct Forall : Query {
 	explicit Forall(const Quantifier& q) : Query{q} {}
 };
 
--- src/rules.y
+++ src/rules.y
@@ -13,19 +13,30 @@ extern int readInputForLexer( char *buffer, int *numBytesRead, int maxBytesToRea
 
 Query* parsedQuery = nullptr;
 int yyerror(char*);
+extern char* yytext;
 Query* ParseQuery(const std::string&);
-%} 
+
+
+// new LogicOperator(yylval.tokenString, *$1, *$3);
+#define NEW_OBJ_W_STR_N(classname, str) classname(str); free(str)
+#define NEW_OBJ_W_STR(classname, str, ...) classname(str, __VA_ARGS__); free(str)
+
+%}
  
 /* Rules output */
 %union {
 	Expression* expression;
 	Quantifier* quantifier;
 	Query*		query;
+    std::string* tokenString;
 }
 /* Rules types */
 %type <expression> phi psi comparable
 %type <quantifier> quantifier
 %type <query> query stmt
+
+%type <tokenString> bool_op comp_op loc lit var_ident
+
 /* Token specifications */
 %token LITERAL VAR_IDENTIFIER LOCATION BOOLEAN_LOGIC_OPERATOR NEGATION COMPARATOR DEADLOCK LPAREN RPAREN
 %token EXISTS FORALL FINALLY GLOBALLY NEXT UNTIL
@@ -35,27 +46,34 @@ Query* ParseQuery(const std::string&);
 /* Rule Section */
 %%
 
-stmt: query				{ parsedQuery = $1; }
+stmt: query				                { parsedQuery = $1; }
 	;
-query: EXISTS quantifier 		{ $$ = new Exists(*$2); }
-	 | FORALL quantifier 		{ $$ = new Forall(*$2); }
+query: EXISTS quantifier 	            { $$ = new Exists(*$2); }
+	 | FORALL quantifier 	            { $$ = new Forall(*$2); }
 	 ;
-quantifier: FINALLY phi			{ $$ = new Finally(*$2); }
-	 	  | GLOBALLY phi	{ $$ = new Globally(*$2); }
-	 	  | NEXT phi 		{ $$ = new Next(*$2); }
-	 	  | UNTIL phi		{ $$ = new Until(*$2); }
+quantifier: FINALLY phi		            { $$ = new Finally(*$2); }
+	 	  | GLOBALLY phi	            { $$ = new Globally(*$2); }
+	 	  | NEXT phi 		            { $$ = new Next(*$2); }
+	 	  | UNTIL phi		            { $$ = new Until(*$2); }
 	 	  ;
-phi: phi BOOLEAN_LOGIC_OPERATOR phi 	{ $$ = new LogicOperator("", *$1, *$3); }
-   | psi 				{ $$ = $1; }
-   | LPAREN phi RPAREN			{ $$ = new SubExpression("()", *$2); }
-   | DEADLOCK 				{ $$ = new Deadlock("deadlock"); }
+phi: phi bool_op phi 	                { $$ = new LogicOperator(*$2, *$1, *$3); }
+   | psi 				                { $$ = $1; }
+   | LPAREN phi RPAREN		            { $$ = new SubExpression("()", *$2); }
+   | DEADLOCK 				            { $$ = new Deadlock("deadlock"); }
    ;
-psi: comparable COMPARATOR comparable 	{ $$ = new Comparator("todo", *$1, *$3); }
-   | LOCATION				{ $$ = new LocationIdentifier("todo"); }
+psi: comparable comp_op comparable 	    { $$ = new Comparator(*$2, *$1, *$3); }
+   | loc				                { $$ = new LocationIdentifier(*$1); }
    ;
-comparable: VAR_IDENTIFIER 		{ $$ = new VariableIdentifier("todo"); }
-		  | LITERAL 		{ $$ = new Literal("todo"); }
+comparable: var_ident 	                { $$ = new VariableIdentifier(*$1); }
+		  | lit 		                { $$ = new Literal(*$1); }
 		  ;
+
+/* This is a stupid hack, and likely a memory leak, but I am running out of time here so... */
+bool_op: BOOLEAN_LOGIC_OPERATOR { $$ = new std::string(*yylval.tokenString); } ;
+comp_op: COMPARATOR             { $$ = new std::string(*yylval.tokenString); } ;
+loc: LOCATION                   { $$ = new std::string(*yylval.tokenString); } ;
+lit: LITERAL                    { $$ = new std::string(*yylval.tokenString); } ;
+var_ident: VAR_IDENTIFIER       { $$ = new std::string(*yylval.tokenString); } ;
 
 %%
 /* Code section */
