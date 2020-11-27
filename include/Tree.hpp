e67ed897d8a95f09243f36f76df38c062f578878
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-11-27 09:33:59 +0100

    I have a better plan

---
+++ include/Tree.hpp
@@ -0,0 +1,41 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+#include <vector>
+#include <functional>
+
+//// N-Tree implementation
+template<typename T>
+struct Tree {
+    T root;
+    std::vector<Tree<T>> children;
+
+    Tree() = delete;
+    Tree(const T& r) : root(r), children{} {}
+    Tree(T&& r) : root{std::forward(r)}, children{} {}
+
+    /** * Moves the provided subtree into this tree's children */
+    inline void emplace(Tree<T>&& t) { children.emplace_back(std::move(t)); }
+    /** * Copies the provided subtree into this tree's children */
+    void insert(const Tree<T>& t) {
+        children.push_back(t);
+    }
+
+    template<typename F>
+    void tree_accumulate(std::function<F(T&, const F&)> f, F start_val) {
+        auto r = f(root, start_val);
+        for(auto& c : children) c.tree_accumulate(f, r);
+    }
+    void tree_apply(std::function<void(T&)> f) {
+        f(root);
+        for(auto& c : children) c.tree_apply(f);
+    }
+};
+
+template<typename T>
+void apply_tree_func_on_non_tree(T* orig, std::function<T*(T*)> getnext, std::function<void(T*)> f) {
+    T* n = getnext(orig);
+    if(n) apply_tree_func_on_non_tree(n, getnext, f);
+    f(orig);
+}
+
+#endif //TREE_HPP
--- include/types.h
+++ include/types.h
@@ -3,128 +3,39 @@
 #include <string>
 #include <utility>
 
-struct Visitor;
-/* Visitable */
-struct Visitable {
-    virtual void accept(const Visitor& visitor) const = 0;
+enum class NodeType_t : unsigned int {
+    Literal, Var, Location, Deadlock,
+    LogicAnd, LogicOr,
+    Negation,
+    CompLess, CompLessEq, CompNeq, CompEq, CompGreater, CompGreaterEq,
+    SubExpr,
+    Finally, Globally, Next, Until,
+    Exists, Forall,
+    UNKNOWN
 };
 
-struct Visitor {
-public:
-    virtual void Print(const Visitable*) const = 0;
-};
+NodeType_t FromString(const std::string& string) {
+    // Comparator
+    if(string == "<") return NodeType_t::CompLess;
+    if(string == "<=") return NodeType_t::CompLessEq;
+    if(string == "!=") return NodeType_t::CompNeq;
+    if(string == "==") return NodeType_t::CompEq;
+    if(string == ">=") return NodeType_t::CompGreaterEq;
+    if(string == ">") return NodeType_t::CompGreater;
+    // Logic and / or
+    if(string == "&&") return NodeType_t::LogicAnd;
+    if(string == "and")return NodeType_t::LogicAnd;
+    if(string == "||") return NodeType_t::LogicOr;
+    if(string == "or") return NodeType_t::LogicOr;
+    return NodeType_t::UNKNOWN; // There is something very wrong, my dude
+}
 
+struct ASTNode {
+    NodeType_t type;
+    std::string token;
 
-struct Expression : Visitable {
-	std::string expression_string;
-	explicit Expression(std::string  str) : expression_string{std::move(str)} {}
-    virtual void accept(const Visitor& visitor) const = 0;
-};
-
-struct Literal : Expression {
-	explicit Literal(const std::string& str) : Expression{str} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct VariableIdentifier : Expression {
-    explicit VariableIdentifier(const std::string& str) : Expression{str} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct LocationIdentifier : Expression {
-	explicit LocationIdentifier(const std::string& str) : Expression{str} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct Deadlock : Expression {
-	explicit Deadlock(const std::string& str) : Expression{str} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct LogicOperator : Expression {
-	enum struct OperatorType {
-		AND, OR, UNKNOWN
-	};
-	OperatorType type;
-	Expression* left,* right;
-	static OperatorType FromString(const std::string& string) {
-        if(string == "&&") return OperatorType::AND;
-		if(string == "and")return OperatorType::AND;
-		if(string == "||") return OperatorType::OR;
-        if(string == "or") return OperatorType::OR;
-		return OperatorType::UNKNOWN; // Something is very wrong, my dude
-	}
-	LogicOperator(const std::string& str, Expression* l, Expression* r) : Expression{str}, type{FromString(str)}, left{l}, right{r} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct NegationOperator : Expression {
-	Expression* subExpression;
-	explicit NegationOperator(Expression* sub) : Expression{"!"}, subExpression{sub} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct Comparator : Expression {
-	enum struct CompareType {
-		LESSTHAN, LESSTHANEQUAL, NOTEQUAL, EQUAL, GREATERTHAN, GREATERTHANEQUAL, UNKNOWN
-	};
-	CompareType type;
-	Expression* left,* right;
-	static CompareType FromString(const std::string& string) {
-        if(string == "<") return CompareType::LESSTHAN;
-        if(string == "<=") return CompareType::LESSTHANEQUAL;
-        if(string == "!=") return CompareType::NOTEQUAL;
-        if(string == "==") return CompareType::EQUAL;
-        if(string == ">=") return CompareType::GREATERTHAN;
-        if(string == ">") return CompareType::GREATERTHANEQUAL;
-		return CompareType::UNKNOWN; // There is something very wrong, my dude
-	}
-	Comparator(const std::string& str, Expression* l, Expression* r) : Expression{str}, type{FromString(str)}, left{l}, right{r} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-/* LPAREN subexpr RPAREN */
-struct SubExpression : Expression {
-	Expression* expr;
-	SubExpression(const std::string& str, Expression* e) : Expression{str}, expr{e} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct Quantifier : Visitable {
-	Expression* expression;
-	explicit Quantifier(Expression* e) : expression{e} {}
-    virtual void accept(const Visitor& visitor) const = 0;
-};
-
-struct Finally : Quantifier {
-	explicit Finally(Expression* e) : Quantifier{e} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-struct Globally : Quantifier {
-	explicit Globally(Expression* e) : Quantifier{e} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-struct Next : Quantifier {
-	explicit Next(Expression* e) : Quantifier{e} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-struct Until : Quantifier {
-	explicit Until(Expression* e) : Quantifier{e} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-
-struct Query : Visitable {
-	Quantifier* quantifier;
-	explicit Query(Quantifier* q) : quantifier{q} {}
-    virtual void accept(const Visitor& visitor) const = 0;
-};
-struct Exists : Query {
-	explicit Exists(Quantifier* q) : Query{q} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
-};
-struct Forall : Query {
-	explicit Forall(Quantifier* q) : Query{q} {}
-    void accept(const Visitor& visitor) const override { visitor.Print(this); }
+    explicit ASTNode(std::string token) : type{FromString(token)}, token{std::move(token)} {}
+    ASTNode(const NodeType_t& type, std::string token) : type{type}, token{std::move(token)} {}
 };
 
 #endif // TYPES_H
