afe7d78aa9737b8bff4a3c07cda9ccb69c505450
Asger Gitz-Johansen <agitzj16@student.aau.dk>
2020-11-27 12:01:22 +0100

    Updated the comments on the demo

--- src/demo.cpp
+++ src/demo.cpp
@@ -7,13 +7,10 @@ extern Tree<ASTNode>* ParseQuery(const std::string&);
 int main() {
     // Query: "Does it, for all paths, always hold that the 'my_test_var' is true?"
 	auto* q = ParseQuery("A G my_test_var == true and other == false");
-
+    // Now visit the tree with either "tree_apply" or "tree_accumulate". These functions visit the tree DFS-style
 	q->tree_apply([](ASTNode& n) {
-        std::cout << "TYPE: " << ConvertToString(n.type) << " - Token: " << n.token << "\n";
+	    // This will simply print the node
+        std::cout << "TYPE: " << ConvertToString(n.type) << ", Token: " << n.token << "\n";
 	});
-
-	if(q)
-	    return 0;
-	else
-	    return 1;
+	delete q; // Make sure to delete the query again. Alternatively, you can wrap it in a std::unique_ptr
 }
